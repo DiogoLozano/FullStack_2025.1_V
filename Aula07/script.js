860bef727ff1d4f3a8e225bcf270747f597ae9db
DiogoLozano <diogo.henrique.lozano@gmail.com>
2025-04-01 16:54:58 -0300

    aula07 / lab 5

---
+++ Aula07/script.js
@@ -0,0 +1,35 @@
+let canvas = document.getElementById("canvas")
+let ctx = canvas.getContext("2d")
+
+//retangulos
+ctx.beginPath();
+ctx.lineWidth = 2
+ctx.fillStyle = "blue";
+ctx.strokeStyle = "red";
+ctx.fillRect(0,0,70,70);
+ctx.strokeRect(70,70,50,50);
+ctx.closePath();
+
+//linhas
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.strokeStyle = "blue"
+ctx.moveTo(200,150);
+ctx.lineTo(120,120);
+ctx.lineTo(60,250);
+ctx.lineTo(200,250);
+ctx.lineTo(200,150);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+//arcos
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = "blue";
+ctx.strokeStyle = "red"
+ctx.arc(200,200,50,0*Math.PI,2*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
