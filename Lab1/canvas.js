e6f9034e91dd725294fd728f0607a46466225420
DiogoLozano <diogo.henrique.lozano@gmail.com>
2025-04-02 18:47:34 -0300

    Update canvas.js

--- Lab1/canvas.js
+++ Lab1/canvas.js
@@ -10,22 +10,36 @@ function quadrado(cor, medidas){
     ctx.closePath();
 }
 quadrado('blue', [0, 0, 50, 50])
-quadrado('red', [250,0,50,50])
+quadrado('red', [250, 0, 50, 50])
 quadrado('aqua', [0, 125, 25, 50])
-quadrado('aqua', [275, 135, 25, 25])
+quadrado('aqua', [275, 138, 25, 25])
 quadrado('yellow', [0, 250, 25, 50])
 quadrado('yellow', [25, 275, 25, 25])
 quadrado('black', [275, 250, 25, 50])
 quadrado('black', [250, 275, 25, 25])
+quadrado('red', [110, 150, 40, 40])
 
-function linha(){
-
+function linha(cor, start, end){
+    const [x, y] = start
+    const [w, z] = end
     ctx.beginPath();
-    ctx.lineWidth = 2;
-    ctx.fillStyle = "blue"
-    ctx.moveTo(0,100);
-    ctx.lineTo(100,100);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = cor
+    ctx.moveTo(x, y);
+    ctx.lineTo(w, z);
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
+}
+linha('blue', [0,0], [150,150])
+linha('red', [300, 0], [150, 150])
+linha('green', [0, 150], [300, 150])
+linha('gray', [150, 150], [150, 300])
+
+function arc(){
+    const []
+    ctx.beginPath();
+    ctx.arc(medidas * Math.PI);
+    ctx.stroke();
+
 }
